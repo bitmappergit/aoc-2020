64c3ff0c85249ac8e13376cb2eb1ebcbc76a10c7
bitmapper <bitmapper85@gmail.com>
2020-12-03 01:55:47 -0400

    day 3 fixes

--- haskell/Day3.hs
+++ haskell/Day3.hs
@@ -1,14 +1,9 @@
-{-# LANGUAGE LambdaCase, BlockArguments #-}
 module Day3 where
 
 import Text.Parsec
 import Text.Parsec.String
 
-data Piece = T | E | O | X
-  deriving (Show, Eq)
-
-type Board = [[Piece]]
-
+linear :: [[a]] -> [Int] -> [Int] -> [a]
 linear l (x : xs) (y : ys)
   | y < length l = ((l !! y) !! x) : linear l xs ys
   | otherwise = []
@@ -20,13 +15,10 @@ occurences a (x : xs)
   | x == a = succ $ occurences a xs
   | otherwise = occurences a xs
 
-parser :: Parser Board
-parser = manyTill parseLine eof
-  where parseLine :: Parser [Piece]
-        parseLine = do
-          c <- manyTill (char '#' <|> char '.') endOfLine
-          pure $ map (\case '#' -> T ; '.' -> E) c
+parser :: Parser [String] 
+parser = manyTill (manyTill (char '#' <|> char '.') endOfLine) eof 
 
+solve :: [String] -> [Int]
 solve board = zipWith (linear new) first second
   where new = map cycle board
         first = [[0..], [0,3..], [0,5..], [0,7..], [0,1..]]
@@ -35,6 +27,6 @@ solve board = zipWith (linear new) first second
 main :: IO ()
 main = do
   Right temp <- parseFromFile parser "../day3.txt"
-  let res = map (occurences T) $ solve temp
+  let res = map (occurences '#') $ solve temp
   print (res !! 1)
   print $ product res
